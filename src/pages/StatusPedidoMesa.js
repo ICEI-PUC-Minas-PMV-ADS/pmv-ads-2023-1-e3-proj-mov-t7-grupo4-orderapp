8c6aa76c66b67c18fddfea50dc61aec4f6d42c3b
thatavieira <thagurgel0909@gmail.com>
2023-06-01 19:04:21 -0300

    implementando bd

--- src/pages/StatusPedidoMesa.js
+++ src/pages/StatusPedidoMesa.js
@@ -1,90 +1,88 @@
-import * as React from 'react';
-import { List, DataTable } from 'react-native-paper';
-import { View, Text, StyleSheet, StatusBar, SafeAreaView } from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faLock } from '@fortawesome/free-solid-svg-icons/faLock';
 import { faUnlock } from '@fortawesome/free-solid-svg-icons/faUnlock';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { useEffect, useState } from 'react';
+import { SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+import { DataTable, List } from 'react-native-paper';
 
-import Header from '../components/Header';
-import Container from '../components/Container';
 import Body from '../components/Body';
+import Container from '../components/Container';
+import Header from '../components/Header';
 
+import { useIsFocused } from '@react-navigation/native';
 
+import * as SQLite from 'expo-sqlite';
 
-
-
-const statusPedido = [
-  {
-    mesa: 1,
-    pedidos: [
-      { id: 1, mesa: 1, pedido: 'P01-001', status: 'Fechado' },
-      { id: 2, mesa: 1, pedido: 'P01-002', status: 'Fechado' },
-      { id: 3, mesa: 1, pedido: 'P01-003', status: 'Fechado' },
-      { id: 4, mesa: 1, pedido: 'P01-004', status: 'Fechado' },
-      { id: 5, mesa: 1, pedido: 'P01-005', status: 'Fechado' },
-      { id: 6, mesa: 1, pedido: 'P01-006', status: 'Aberto' },
-    ],
-  },
-  {
-    mesa: 2,
-    pedidos: [
-      { id: 1, mesa: 2, pedido: 'P02-001', status: 'Fechado' },
-      { id: 2, mesa: 2, pedido: 'P02-002', status: 'Fechado' },
-      { id: 3, mesa: 2, pedido: 'P02-003', status: 'Fechado' },
-      { id: 4, mesa: 2, pedido: 'P02-004', status: 'Fechado' },
-      { id: 5, mesa: 2, pedido: 'P02-005', status: 'Fechado' },
-      { id: 6, mesa: 2, pedido: 'P02-006', status: 'Aberto' },
-    ],
-  },
-  {
-    mesa: 3,
-    pedidos: [
-      { id: 1, mesa: 3, pedido: 'P03-001', status: 'Fechado' },
-      { id: 2, mesa: 3, pedido: 'P03-002', status: 'Fechado' },
-      { id: 3, mesa: 3, pedido: 'P03-003', status: 'Fechado' },
-      { id: 4, mesa: 3, pedido: 'P03-004', status: 'Fechado' },
-      { id: 5, mesa: 3, pedido: 'P03-005', status: 'Fechado' },
-      { id: 6, mesa: 3, pedido: 'P03-006', status: 'Aberto' },
-    ],
-  },
-];
+const db = SQLite.openDatabase('ohchefia.db');
 
 const StatusPedidoMesa = () => {
+
+  const isFocused = useIsFocused();
+
+  const [mesas, setMesas] = useState([]);
+
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql('SELECT DISTINCT nummesa FROM mesas', [], (_, { rows }) => {
+        const mesas = rows._array.map((item) => ({
+          nummesa: item.nummesa,
+          pedidos: [],
+        }));
+        setMesas(mesas);
+      });
+      tx.executeSql('SELECT * FROM pedidos', [], (_, { rows }) => {
+        const pedidos = rows._array;
+        setMesas((prevMesas) =>
+          prevMesas.map((mesa) =>
+            Object.assign({}, mesa, {
+              pedidos: pedidos
+                .filter((pedido) => pedido.nummesa === mesa.nummesa)
+                .map((pedido, index) =>
+                  Object.assign({}, pedido, { sequencia: index + 1 })
+                ),
+            })
+          )
+        );
+      });
+    });
+  }, [isFocused]);
+
   return (
     <Container>
-    <Header title={'PEDIDOS'} />
-    <Body>
-    <SafeAreaView style={styles.container}>
-    <List.Section>
-      {statusPedido.map((item) => (
-        <List.Accordion title={`Pedidos - Mesa  ${item.mesa}`}>
-          <View>
-            <DataTable>
-              <DataTable.Header>
-                <DataTable.Title>Pedido</DataTable.Title>
-                <DataTable.Title numeric>Status</DataTable.Title>
-                <DataTable.Title numeric></DataTable.Title>
-              </DataTable.Header>
-              {item.pedidos.map((p) => (
-                <DataTable.Row>
-                  <DataTable.Cell>{p.pedido}</DataTable.Cell>
-                  <DataTable.Cell numeric>{p.status}</DataTable.Cell>
-                  <DataTable.Cell numeric>
-                    {p.status === 'Fechado' ? (
-                      <FontAwesomeIcon icon={faLock} />
-                    ) : (
-                      <FontAwesomeIcon icon={faUnlock} />
-                    )}
-                  </DataTable.Cell>
-                </DataTable.Row>
-              ))}
-            </DataTable>
-          </View>
-        </List.Accordion>
-      ))}
-    </List.Section>
-    </SafeAreaView>
-    </Body>
+      <Header title={'PEDIDOS'} />
+      <Body>
+        <SafeAreaView style={styles.container}>
+          <List.Section>
+            {mesas.map((item) => (
+              <List.Accordion title={`Pedidos - Mesa  ${item.nummesa}`}>
+                <View>
+                  <DataTable>
+                    <DataTable.Header>
+                      <DataTable.Title>Pedido</DataTable.Title>
+                      <DataTable.Title numeric>Status</DataTable.Title>
+                      <DataTable.Title numeric></DataTable.Title>
+                    </DataTable.Header>
+                    {item.pedidos.map((p) => (
+                      <DataTable.Row>
+                        <DataTable.Cell>{`P0${p.nummesa}-00${p.id}`}</DataTable.Cell>
+                        <DataTable.Cell numeric>{p.status === null ? 'Aberto' : p.status}</DataTable.Cell>
+                        <DataTable.Cell numeric>
+                          {p.status === 'Fechado' ? (
+                            <FontAwesomeIcon icon={faLock} />
+                          ) : (
+                            <FontAwesomeIcon icon={faUnlock} />
+                          )}
+                        </DataTable.Cell>
+                      </DataTable.Row>
+                    ))}
+                  </DataTable>
+                </View>
+              </List.Accordion>
+            ))}
+          </List.Section>
+        </SafeAreaView>
+      </Body>
     </Container>
   );
 };
