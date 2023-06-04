4be801ca613641810c22edccfc0f849c87a355b6
Vinicius-S-P <vinicius-sponciano@hotmail.com>
2023-06-04 14:03:30 -0300

    updating packages

--- src/pages/cardapio/AdicionaCardapio.js
+++ src/pages/cardapio/AdicionaCardapio.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, Image, StyleSheet, Alert } from 'react-native';
 import {
   RadioButton,
   Text,
